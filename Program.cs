2c48d012bca0dda3f76839996bcdf5a4f600f175
juescoryisus <125850604+juescoryisus@users.noreply.github.com>
2023-09-01 08:57:28 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace recursividad_en_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\jesus\OneDrive\Escritorio\MUSICA"; // Cambia esto a la ruta de tu directorio raíz
+            ListFilesRecursively(path);
+        }
+
+        static void ListFilesRecursively(string directory)
+        {
+            foreach (string file in Directory.GetFiles(directory))
+            {
+                Console.WriteLine($"Archivo: {Path.GetFileName(file)}, Extensión: {Path.GetExtension(file)}");
+                Console.WriteLine("==================================================================================================================");
+            }
+
+            foreach (string subdirectory in Directory.GetDirectories(directory))
+            {
+                ListFilesRecursively(subdirectory);
+            }
+            Console.ReadKey();
+        }
+    }
+}
